80ddd56a2c3d2ea72ec7c06c69d942c9f2b400f2
tokhirboiskhonov <tokhir060@gmail.com>
2024-03-18 20:34:00 +0500

    first step: select needed tags using document.querySelector from HTML.

--- Practice-dom/js/script.js
+++ Practice-dom/js/script.js
@@ -1,6 +1,7 @@
-const promoAdv = document.querySelector(".promo__adv"),
+const promoAdvs = document.querySelectorAll(".promo__adv img"),
   promoGenre = document.querySelector(".promo__genre"),
-  promoBgImg = document.querySelector(".promo__bg");
+  promoBgImg = document.querySelector(".promo__bg"),
+  promoList = document.querySelector(".promo__interactive-list");
 // remove advertising block
 promoAdv.remove();
 
